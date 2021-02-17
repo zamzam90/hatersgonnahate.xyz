07da3dfa01732654f76fa6c4f8274e4402eeb6fc
zamzam90 <siltanen.sami@hotmail.com>
2021-02-17 10:36:18 +0200

    moar JS :D

--- JS/testing3.js
+++ JS/testing3.js
@@ -8,3 +8,15 @@ function HELLO()
 {
   alert("HELLO WORLD!!");
 }
+//reversing a word, maybe..
+function reverse()
+{
+  var haettuTeksti = document.getElementById('reversedwrd').value;
+  var sananPituus = haettuTeksti.length;
+  var uusiSana = '';
+  for(var i = sananPituus-1; i >= 0; i--)
+  {
+    uusiSana = uusiSana + haettuTeksti[i];
+  }
+  document.getElementById('result').innerHTML = uusiSana;
+}
